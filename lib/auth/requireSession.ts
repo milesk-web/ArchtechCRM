4acde7281eae453b891e68770aa133d68c71426b
milesk-web <milesk@archtech.co.nz>
2026-09-16 05:59:19 +1200

    Update requireSession.ts

--- lib/auth/requireSession.ts
+++ lib/auth/requireSession.ts
@@ -1,1 +1,12 @@
+import { redirect } from "next/navigation";
+import { getSession } from "./session";
 
+export async function requireSession() {
+  const session = await getSession();
+
+  if (!session?.user) {
+    redirect("/login");
+  }
+
+  return session;
+}
